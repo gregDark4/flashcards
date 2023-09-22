89beae4da04de42a128216fa571a61e8cfa690ee
albochkor <albochkor@gmail.com>
2023-09-22 13:13:28 +0300

    controller

---
+++ function.js
@@ -0,0 +1,37 @@
+const { question } = require("readline-sync");
+const process = require('process');
+
+
+class Template {
+    constructor (this.questions, theme) {
+    //?
+    this.theme=theme;
+    }
+
+getQ() {
+    let aque = [];
+    for (let i=0; i<aque; i+=1) {
+        aque.push(this.questions.q);
+    }
+    return aque
+}
+}
+
+
+function answering (q) {
+let answers = prompt(`${q}${aque[i]}`, answer)
+return answers;
+}
+
+function checking (answering(q)) {
+    let trial = answers;
+if (answers===this.aque[i]) {
+    scores+=50;
+    console.log('+ bowl of rice')
+} else {
+    console.log('непральна:(')
+}
+//next q
+}
+
+q3: jbdjklwdenln
